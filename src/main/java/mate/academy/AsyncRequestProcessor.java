c133aa77f65d8b91ad339eb64aeb0d190c369d7a
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-12-28 21:10:25 +0100

    impl

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,30 @@
 package mate.academy;
 
+import java.time.LocalTime;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private final Map<String, UserData> cachedUsers = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() ->
+                cachedUsers.computeIfAbsent(userId, this::loadUser), executor);
+    }
+
+    private UserData loadUser(String userId) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return new UserData(userId, "Details.");
     }
 }
